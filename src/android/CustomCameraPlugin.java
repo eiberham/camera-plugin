6a55c2b6192e66bd725d27251e91bbd0d8768365
Abraham Cedeno <acedeno@zetech.com.ar>
2017-05-18 15:24:35 -0300

    images action fix

--- src/android/CustomCameraPlugin.java
+++ src/android/CustomCameraPlugin.java
@@ -66,7 +66,7 @@ public class CustomCameraPlugin extends CordovaPlugin{
             Log.i("XXX", "Pasa por imágenes");
             try {
                 //JSONObject json = new JSONObject(args.getString(0));
-                JSONArray json = args.getJSONArray(0);
+                JSONObject json = args.getJSONObejct(0);
                 JSONObject images = json.has("images") ? json.getJSONObject("images") : null;
                 Log.i("XXX", images.toString());
                 /*for(int i=0; i<json.length(); i++){
